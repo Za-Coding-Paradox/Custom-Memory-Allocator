7ddd13e5592d2e7b37f6f319df793bbb3d5426c3
Za-Coding-Paradox <zacodingparadox@gmail.com>
2026-01-31 21:54:53 +0500

    Revoking Handle usage by linear allocator

--- include/modules/strategies/linear_module/linear_scopes.h
+++ include/modules/strategies/linear_module/linear_scopes.h
@@ -6,14 +6,17 @@ namespace Allocator {
 
 struct FrameLoad {
   static constexpr bool IsRewindable = false;
+  static constexpr bool SupportsHandles = false;
 };
 
 struct LevelLoad {
   static constexpr bool IsRewindable = true;
+  static constexpr bool SupportsHandles = false;
 };
 
 struct GlobalLoad {
   static constexpr bool IsRewindable = true;
+  static constexpr bool SupportsHandles = false;
 };
 
 } // namespace Allocator
